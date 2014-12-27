e48dfb7af8ce00d1a98a5d9543f6b2db9916b19f
xantrion Puppet-managed User <xantrion@xan-den-rh01.xantrion-hq.local>
2014-12-26 17:21:12 -0700

    Added the ability to send an e-mail with the results

---
+++ email.rb
@@ -0,0 +1,56 @@
+def send_email() 
+require 'net/smtp'
+
+filename = "expiration_report.txt"
+# Read a file and encode it into base64 format
+filecontent = File.read(filename)
+encodedcontent = [filecontent].pack("m") 	#base 64
+
+marker = "AUNIQUEMARKER"
+
+body =<<EOF
+This is a sample automated e-mail that could be sent on a regular interval.
+
+EOF
+
+# Define the main headers.
+part1 =<<EOF
+From: Certificate Bot <no-reply@xantrion.com>
+To: Patrick Carpenter <pcarpenter@xantrion.com>
+Subject: Certificate Expiry Report
+MIME-Version: 1.0
+Content-Type: multipart/mixed; boundary=#{marker}
+--#{marker}
+EOF
+
+# Define the message action
+part2 =<<EOF
+Content-Type: text/plain
+Content-Transfer-Encoding:8bit
+
+#{body}
+--#{marker}
+EOF
+
+# Define the attachment section
+part3 =<<EOF
+Content-Type: multipart/mixed; name=\"#{filename}\"
+Content-Transfer-Encoding:base64
+Content-Disposition: attachment; filename="#{filename}"
+
+#{encodedcontent}
+--#{marker}--
+EOF
+
+mailtext = part1 + part2 + part3
+
+# Let's put our code in safe area
+begin 
+  Net::SMTP.start('xan-den-ex01.xantrion-hq.local') do |smtp|
+       smtp.sendmail(mailtext, 'no-reply@xantrion.com',
+                                 ['pcarpenter@xantrion.com'])
+  end
+rescue Exception => e  
+    print "Exception occured: " + e  
+end  
+end
--- master.rb
+++ master.rb
@@ -1,7 +1,32 @@
 #~/usr/bin/ruby
 require 'certcheck.rb'
+require 'email.rb'
 
+time = Time.now.getutc
+
+# clear the old log files
+File.open('https.log', 'w') {|file| file.truncate(0) }
+File.open('https.expire', 'w') {|file| file.truncate(0) }
+#File.open('expiration_report.txt', 'w') {|file| file.truncate(0) }
+File.open('expiration_report.txt', 'w') {|file| file.write("This is an automated report.  It was generated on #{time} \r\n \r\nPlease contact the Xantrion NOC if you have any questions.\r\n\r\nThe following SSL Certs are due to expire in the next 30 days:\r\n\r\n") }
+
+# open the text file and pass each name to the cert check method
 text=File.read("sites.txt").split("\n")
 text.each do |sitename|
 	certificate_check("#{sitename}")
 end
+
+# Open the log file
+#
+#results=File.read("https.log").split("\n")
+
+expiring_input = File.read('https.expire')
+output = File.open('expiration_report.txt', 'a')
+output.write(expiring_input)
+output.write("-----------------------\r\n\r\n\r\n\r\n\r\nThe following certs were tested:\r\n\r\n")
+log_input = File.read('https.log')
+output.write(log_input)
+output.close
+send_email
+
+	
