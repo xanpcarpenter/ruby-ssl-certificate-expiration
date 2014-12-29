da0f716388dbdf6d28ef1b94e36a3c77a021c9db
xantrion Puppet-managed User <xantrion@xan-den-rh01.xantrion-hq.local>
2014-12-29 09:10:47 -0700

    Fixed an issue where a cert in the expiry range would cause the program to exit

--- certcheck.rb
+++ certcheck.rb
@@ -9,6 +9,7 @@ def certificate_check (arg1)
 	http = Net::HTTP.new(uri.host,uri.port) #removed proxy variables methods
 	http.use_ssl = true
 	http.verify_mode = OpenSSL::SSL::VERIFY_NONE
+	puts "Connecting to #{url}"
 	http.start do |h|
   	@cert = h.peer_cert
 	end
@@ -28,16 +29,14 @@ def certificate_check (arg1)
 	# Open the log file and record the days remaining for each certificate checked.
 	days_remaining = @cert.not_after.to_i - Time.now.to_i
 	File.open('https.log', 'a') do |f|
-		f.write "The certificate for #{url} expires in #{days_remaining / (60*60*24).to_i} days.\n"
+		f.write "The certificate for #{url} expires in #{days_remaining / (60*60*24).to_i} days.\r\n"
 	end
 
 	# check if cert expire time is less than expire_within_days. Fail if true and write to expire file.
 	if @cert.not_after.to_i < (Time.now + (expire_within_days * 24*60*60)).to_i
   		File.open('https.expire', 'a') do |f|
-		f.write "The certificate for #{url} expires in #{days_remaining / (60*60*24).to_i} days on #{@cert.not_after}.\n"
-  	end
-
-#  puts "Certificate expires at: #{@cert.not_after}"
-  	exit 1
+		f.write "The certificate for #{url} expires in #{days_remaining / (60*60*24).to_i} days on #{@cert.not_after}.\r\n"
+  		end
 	end
+
 end
